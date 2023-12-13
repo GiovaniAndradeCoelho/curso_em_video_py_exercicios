637bae94b2c920f7320f3a317000086f581f8e89
GiovaniAndradeCoelho <giovani.andradesolutions@gmail.com>
2023-12-13 15:36:51 -0300

    Exercicio 001

---
+++ 001.py
@@ -0,0 +1,9 @@
+# Criar uma função que retorne 'Hello World!'
+
+def hello_world():
+    return 'Hello World!'
+
+print(hello_world())
+
+# Exercicio 001 - Resolvido
+# Aula https://www.youtube.com/watch?v=nIHq1MtJaKs&list=PLHz_AreHm4dlKP6QQCekuIPky1CiwmdI6&index=7
